627696f57af202f2f01fc37d5fc83af79e953cdc
Arjun A Acharry <arjunaacharry007@gmail.com>
2024-01-25 19:13:07 +0530

    style Added

---
+++ script.js
@@ -0,0 +1,8 @@
+// Get the button element
+var btn = document.querySelector(".btn");
+
+// Add a click event listener to the button
+btn.addEventListener("click", function() {
+  // Open the projects page in a new tab
+  window.open("#", "_blank");
+});
